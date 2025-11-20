d9061912a8cc489158a091bb3a95b4a75f04c6fa
mahonrri-eng <mahonrri@gmail.com>
2025-11-20 13:12:27 -0300

    diseño final + íconos PWA  mejoras en gráfico y recomendaciones

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,12 @@
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('presupuesto-cache').then(cache => {
-            return cache.addAll(['/', '/index.html', '/styles.css', '/script.js']);
-        })
-    );
+  e.waitUntil(
+    caches.open('presupuesto-cache').then(cache => cache.addAll([
+      '/', '/index.html', '/styles.css', '/script.js', '/manifest.json',
+      '/icons/icon-192.png', '/icons/icon-512.png', '/icons/icon-180.png'
+    ]))
+  );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
